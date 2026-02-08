e73b3754fe30a563e5780e9ae44c1e91ba27faa8
FrankDeGroot <fjtdg@hotmail.com>
2026-02-08 15:03:03 +0100

    + auth api

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,5 +1,12 @@
 module.exports = async function (context, req) {
-  context.res = {
-    body: "Hello from Azure Function API!"
-  };
+  const clientPrincipal = req.headers['x-ms-client-principal'];
+  if (!clientPrincipal) {
+    // context.res = { status: 401, body: "Not authenticated" };
+    context.res = {
+      body: "Hello from Azure Function API!"
+    };
+    return;
+  }
+  const decoded = JSON.parse(Buffer.from(clientPrincipal, 'base64').toString('ascii'));
+  context.res = { body: `Hello ${decoded.userDetails}, you logged in with ${decoded.identityProvider}` };
 };
