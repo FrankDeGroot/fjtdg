97a7f381fb67571a5c347dddee8b236aac021f09
FrankDeGroot <fjtdg@hotmail.com>
2026-03-05 07:58:56 +0100

    mv admin boss

--- api/boss/index.js
+++ api/boss/index.js
@@ -3,10 +3,10 @@ module.exports = async function (context, req) {
   if (!clientPrincipal) {
     // context.res = { status: 401, body: "Not authenticated" };
     context.res = {
-      body: "(admin) Hello from Azure Function API!"
+      body: "(boss) Hello from Azure Function API!"
     };
     return;
   }
   const decoded = JSON.parse(Buffer.from(clientPrincipal, 'base64').toString('ascii'));
-  context.res = { body: `(admin) Hello ${decoded.userDetails}, you logged in with ${decoded.identityProvider}` };
+  context.res = { body: `(boss) Hello ${decoded.userDetails}, you logged in with ${decoded.identityProvider}` };
 };
